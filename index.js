7a69dc36bb16004e41f0392cb77f118f6629fc56
ensar <ensarsagkal@gmail.com>
2024-03-16 14:06:09 +0300

    feat:structure

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+/* -------------------------------------------------------
+CLARUS STORE
+------------------------------------------------------- */
+const express=require("express")
+const app=express()
+
+app.use(express.json())
+
+require("dotenv").config()
+ const PORT=process.env.PORT
+ const HOST=process.env.HOST
+
+
+
+ app.listen(PORT, () => console.log(` Server Running on http://${HOST}:${PORT}`))
