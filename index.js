8b8988a2b0a1220213286c1e0822ca2538fe29d5
ensar <ensarsagkal@gmail.com>
2024-03-16 14:18:08 +0300

    feat:errHandler

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 CLARUS STORE
 ------------------------------------------------------- */
 const express=require("express")
-const app=express()
+const app=express()     //*use express
 
 app.use(express.json())
 
 require("dotenv").config()
  const PORT=process.env.PORT
  const HOST=process.env.HOST
-require("./src/db")
+require("./src/db")     //*db connection
 
+
+app.use(require("./src/errorHandler"))    //* errorhandler 
 
  app.listen(PORT, () => console.log(` Server Running on http://${HOST}:${PORT}`))
--- src/db.js
+++ src/db.js
@@ -2,4 +2,4 @@ const mongoose=require("mongoose")
 const MONGODB=process.env.MONGODB
 mongoose.connect(MONGODB)
 .then(()=> console.log("**DB CONNECTED**"))
-.catch(()=> console.log("!! DB not CONNECTED!!"))
+.catch(()=> console.log("!! DB not CONNECTED!!",err))
